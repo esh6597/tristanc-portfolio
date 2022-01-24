fe91632138a35fbc07934438feadfb36623e6410
Erin Han <eshan6597@gmail.com>
2022-01-24 13:44:48 -0700

    Add form to contact section; add prelim routes to server

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const express = require('express');
 //Define app
 const app = express();
 const PORT = process.env.PORT || 3000;
+const router = express.Router();
 
-//Serve static assets in /public
-/**
- * Note that this doesn't throw an error if the domain isn't found; instead it uses next()
- */
 app.use(express.static('public'));
+
+//Basic routing
+router.route('/email')
+.get((req, res, next) => {
+  res.statusCode = 403;
+  res.end('GET operation not allowed!');
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not allowed!');
+})
+.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not allowed!');
+})
+.delete((req, res, next) => {
+  res.statusCode = 403;
+  res.end('DELETE operation not allowed!');
+});
 
 //Deploy server
 app.listen(PORT, () => {
