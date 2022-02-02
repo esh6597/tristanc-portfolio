a41944bccfe083a844053162ae8f96a7dadc23af
Erin Han <eshan6597@gmail.com>
2022-02-02 15:10:59 -0700

    Add daily email quantity limit to app

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 //Define + config mailing capabilities
 import mailService from 'sib-api-v3-sdk';
 // const mailService = require('sib-api-v3-sdk');
@@ -35,7 +34,7 @@ apiKey.apiKey = key;
 let apiInstance = new mailService.TransactionalEmailsApi();
 
 //Define mail protocol
-let sendMail = (firstName, 
+const sendMail = async (firstName, 
   lastName, 
   email, 
   phone, 
@@ -43,24 +42,55 @@ let sendMail = (firstName,
   message, 
   callback) => {
 
-  //Format data into email content
-  const fullName = firstName + ' ' + lastName;
-  const fullHTML = '<p>' + message + "</p><br /><br /><p>Sent by Node Mailer and MailGun | Contact this email's sender at " + phone;
+  //Read database file and set data
+  await db.read();
 
-  let sendSmtp = new mailService.SendSmtpEmail();
+  //If missing set default data
+  db.data ||= {
+    "emails": 0,
+    "cutoff": 300,
+    "lastUpdate": Date.now()
+  };
 
-  sendSmtp.sender = {'name': fullName, 'email': email};
-  sendSmtp.replyTo = {'name': fullName, 'email': email};
-  sendSmtp.to = [recipient];
-  sendSmtp.subject = subject;
-  sendSmtp.htmlContent = fullHTML;
+  let currentDate = Date.now();
 
-  apiInstance.sendTransacEmail(sendSmtp)
-  .then(function (data) {
-    callback(null, data);
-  }, function (error) {
+  //Longer than 24 hrs since last refresh
+  if ( (currentDate - db.data.lastUpdate) >= 86400000 ) {
+    //Update database
+    db.data.lastUpdate = currentDate;
+    db.data.emails = 0;
+  }
+
+  if (db.data.emails < db.data.cutoff) {
+    //Format data into email content
+    const fullName = firstName + ' ' + lastName;
+    const fullHTML = '<p>' + message + "</p><br /><br /><p>Sent by Node Mailer and MailGun | Contact this email's sender at " + phone + "<br />Email " + (db.data.emails + 1) + " out of 300 today";
+
+    let sendSmtp = new mailService.SendSmtpEmail();
+
+    sendSmtp.sender = {'name': fullName, 'email': email};
+    sendSmtp.replyTo = {'name': fullName, 'email': email};
+    sendSmtp.to = [recipient];
+    sendSmtp.subject = subject;
+    sendSmtp.htmlContent = fullHTML;
+
+    
+    apiInstance.sendTransacEmail(sendSmtp)
+    .then(async function (data) {
+      //Successful
+      callback(null, data);
+      db.data.emails += 1;
+      await db.write();
+
+    }, async function (error) {
+      //Error
+      callback(err, null);
+      await db.write();
+    });
+  } else {
+    console.log('Sorry, daily email limit reached.');
     callback(err, null);
-  });
+  }
 }
 
 
