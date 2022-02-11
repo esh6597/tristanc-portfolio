e0e8281cbdb559d830f9e3d16e97fa2c2b638e4f
Erin Han <eshan6597@gmail.com>
2022-02-11 13:47:30 -0700

    Form validation part 2: add validation functions

--- public/form.js
+++ public/form.js
@@ -1,24 +1,71 @@
 // || FORM VALIDATION
 window.addEventListener('DOMContentLoaded', event => {
-  const form = document.querySelector('#contactForm');
-  form.addEventListener('change', () => {
-    const controls = document.querySelectorAll('.form-control');
-    let isValid = true;
-    controls.forEach((input) => {
-    });
+  const firstNInp = document.querySelector('#firstName');
+  const lastNInp = document.querySelector('#lastName');
+  const emailInp = document.querySelector('#email');
+  const phoneInp = document.querySelector('#phone');
+  const subjectInp = document.querySelector('#subject');
+  const messageInp = document.querySelector('#message');
+  const checkbox = document.querySelector('#privacyCheck');
+  const submitBut = document.querySelector('#submitBut');
+
+  // VALIDATOR FX'S--reuseable
+  // HTML has its own validation API, but custom JS validation offers better freedom and code organization. To allow special characters in names, we will not be validating names beyond the no angle bracket rule.
+
+  const notEmpty = (val) => {
+    return val.toString().length > 0 ? true : false;
+  };
+
+  const noAngleBr = (val) => {
+    //Tests both literal symbol and HTML entities
+    const conditions = ['&lt;', '<', '&gt;', '>'];
+    return conditions.some(con => val.toString.includes(con));
+  };
+
+  const validEmail = (val) => {
+    //Regex matches:
+    // Beginning with any characters to allow unconventional email addresses then @ symbol required
+    // Any more characters again, then period and 2-3 letters for end
+    if (val.toString().match(/^.+@.+\.\w{2,3}$/)) {
+      //Cannot be longer than 320 characters universally
+      if (val.toString().length <= 320) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const validPhone = (val) => {
+    // Match 0123456789 or 012-345-6789
+    if (val.toString().match(/^\d{10}$/) || val.toString().match(/^\d{3}-\d{3}-\d{4}$/)) {
+      return true;
+    }
+    return false;
+  };
+
+  const validSubj = (val) => {
+    return val.toString().length <= 160 ? true : false;
+  };
+
+  // ASSIGN CHANGES UPON INPUT
+
+  firstNInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${firstNInp.id}Feedback`);
+    
   });
 });
 
-// add change event listener to entire form
-  // check if all fields are valid via class
-  // if valid, enable submit button
-  // else disable submit button
+// query select all fields and submit button
+  
 
 // add change event listener to fields
   // validate field
   // get feedback div
   // if valid update classes and inner html
   // if invalid do the same but different :^)
+  // check if all fields are valid via class
+  // if valid, enable submit button
+  // else disable submit button
 
 // || FORM SUBMISSION
 
