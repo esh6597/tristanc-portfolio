bd82a8a113b249b89cc7b15646abebc3bfc323e4
Erin Han <eshan6597@gmail.com>
2022-02-11 14:46:53 -0700

    Add validation functions via JS

--- public/form.js
+++ public/form.js
@@ -1,5 +1,6 @@
 // || FORM VALIDATION
 window.addEventListener('DOMContentLoaded', event => {
+  const form = document.querySelector('#contactForm');
   const firstNInp = document.querySelector('#firstName');
   const lastNInp = document.querySelector('#lastName');
   const emailInp = document.querySelector('#email');
@@ -8,6 +9,9 @@ window.addEventListener('DOMContentLoaded', event => {
   const messageInp = document.querySelector('#message');
   const checkbox = document.querySelector('#privacyCheck');
   const submitBut = document.querySelector('#submitBut');
+
+  //Disable submission automatically
+  submitBut.disabled = true;
 
   // VALIDATOR FX'S--reuseable
   // HTML has its own validation API, but custom JS validation offers better freedom and code organization. To allow special characters in names, we will not be validating names beyond the no angle bracket rule.
@@ -19,7 +23,7 @@ window.addEventListener('DOMContentLoaded', event => {
   const noAngleBr = (val) => {
     //Tests both literal symbol and HTML entities
     const conditions = ['&lt;', '<', '&gt;', '>'];
-    return conditions.some(con => val.toString.includes(con));
+    return conditions.some(con => val.toString().includes(con))? false : true;
   };
 
   const validEmail = (val) => {
@@ -43,29 +47,186 @@ window.addEventListener('DOMContentLoaded', event => {
     return false;
   };
 
-  const validSubj = (val) => {
+  const validSubject = (val) => {
     return val.toString().length <= 160 ? true : false;
+  };
+
+  const validMessage = (val) => {
+    //Limiting space to short messages for faster processing
+    return val.toString().length <= 10000 ? true : false;
   };
 
   // ASSIGN CHANGES UPON INPUT
 
+  //State variables for if form is valid
+  let firstNVal = false;
+  let lastNVal = false;
+  let emailVal = false;
+  let phoneVal = false;
+  let subjectVal = false;
+  let messageVal = false;
+  let checkVal = false;
+  let formVal = false;
+
   firstNInp.addEventListener('input', () => {
     const feedback = document.querySelector(`#${firstNInp.id}Feedback`);
     
+    // Validate: required, no <>
+    if (notEmpty(firstNInp.value) && noAngleBr(firstNInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      firstNVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Oops, please enter a valid name.';
+      firstNVal = false;
+    }
   });
-});
 
-// query select all fields and submit button
-  
+  lastNInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${lastNInp.id}Feedback`);
+    
+    // Validate: required, no <>
+    if (notEmpty(lastNInp.value) && noAngleBr(lastNInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      lastNVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Please enter a valid name.';
+      lastNVal = false;
+    }
+  });
+
+  emailInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${emailInp.id}Feedback`);
+    
+    // Validate: required, is a valid email
+    if (notEmpty(emailInp.value) && validEmail(emailInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      emailVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Please enter a valid email.';
+      emailVal = false;
+    }
+  });
+
+  phoneInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${phoneInp.id}Feedback`);
+    
+    // Validate: is a valid phone format
+    if (validPhone(phoneInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      phoneVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Oops, please enter a valid phone number.';
+      phoneVal = false;
+    }
+  });
+
+  subjectInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${subjectInp.id}Feedback`);
+    
+    // Validate: is a valid phone format
+    if (notEmpty(subjectInp.value) && validSubject(subjectInp.value) && noAngleBr(subjectInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      subjectVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Oops, please keep your subject under 160 characters.';
+      subjectVal = false;
+    }
+  });
+
+  messageInp.addEventListener('input', () => {
+    const feedback = document.querySelector(`#${messageInp.id}Feedback`);
+    
+    // Validate: is a valid phone format
+    if (notEmpty(messageInp.value) && validMessage(messageInp.value) && noAngleBr(messageInp.value)) {
+      //Does nothing if class doesn't exist
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Looks good!';
+      messageVal = true;
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'Oops, please enter a message under 10000 characters. For security reasons, angle brackets (\< and \>) are not allowed.';
+      messageVal = false;
+    }
+  });
+
+  checkbox.addEventListener('change', () => {
+    const feedback = document.querySelector(`#${checkbox.id}Feedback`);
+    if (checkbox.checked) {
+      feedback.classList.remove('invalid');
+      //Add custom styling
+      feedback.classList.add('valid');
+      feedback.innerHTML = 'Thank you for protecting both of our privacies.';
+    } else {
+      //Not valid
+      feedback.classList.remove('valid');
+      feedback.classList.add('invalid');
+      feedback.innerHTML = 'To submit your message, you must agree with this statement.';
+    }
+    checkVal = checkbox.checked;
+  });
+
+  //Form checks state upon every change and toggles submit button
+
+  form.addEventListener('change', () => {
+    if (firstNVal 
+      && lastNVal 
+      && emailVal 
+      && phoneVal 
+      && subjectVal 
+      && messageVal 
+      && checkVal) {
+      submitBut.disabled = false;
+    } else {
+      submitBut.disabled = true;
+    }
+  });
+
+});
 
 // add change event listener to fields
   // validate field
   // get feedback div
   // if valid update classes and inner html
   // if invalid do the same but different :^)
-  // check if all fields are valid via class
-  // if valid, enable submit button
-  // else disable submit button
 
 // || FORM SUBMISSION
 
