0c2d0673c2fb88e3a3d34c04b0ccf2343813b247
Erin Han <eshan6597@gmail.com>
2022-02-16 10:09:39 -0700

    Separate gallery scripts into its own file

--- public/scripts.js
+++ public/scripts.js
@@ -98,46 +98,4 @@ window.addEventListener('DOMContentLoaded', event => {
 
     masthead.addEventListener('mouseenter', moveOn);
     masthead.addEventListener('mouseleave', moveOff);
-
-
-    // || GALLERY FADE ANIM
-    const gallery = document.querySelector('.opacity-in');
-    if (gallery) {
-        gallery.style.opacity = 1;
-    }
-
-
-    // || GALLERY MODALS
-    const zoomView = document.querySelector('.gallery-zoom');
-    const slideshow = document.querySelector('.carousel');
-
-    if (zoomView && slideshow) {
-
-        //Clicking on gallery cards scrolls carousel to relevant image
-        const galleryCards = document.querySelectorAll('.gallery-card');
-        galleryCards.forEach((card) => {
-            card.addEventListener('click', () => {
-                zoomView.style.display = 'block';
-                document.querySelector(`#b-${card.id}`).click();
-            });
-        });
-
-        //Close carousel overlay
-        const exit = document.querySelector('.exit');
-        exit.addEventListener('click', () => {
-            zoomView.style.display = 'none';
-        });
-    }
-});
-
-// || PREVENT AUTOSCROLL
-// Must be in jQuery to work.
-
-//Begin jQuery
-$(function() {
-    $('.carousel').each(function(){
-        $(this).carousel({
-            interval: false
-        });
-    });
 });
